6aeb5c243a2dd7adfebfe98717bc82d4edc0c076
Steven Roose <stevenroose@gmail.com>
2016-12-12 17:10:40 +0100

    Add check for illegal base64url characters

--- base64url_test.go
+++ base64url_test.go
@@ -1,26 +1,27 @@
-package base64url
+package base64url_test
 
 import (
-	"github.com/kalaspuffar/base64url"
-	"testing"
 	"encoding/hex"
+	"testing"
+
+	"github.com/kalaspuffar/base64url"
 )
 
 var (
-	HEXEncoded = "69b73efeba"
+	HEXEncoded       = "69b73efeba"
 	Base64URLEncoded = "abc-_ro"
 )
 
 /*
 	Test base64url.Encode function using a hex encoded string corresponding to an Base64URLEncoded string.
-	First we decode the hex encoded bytes and then run the encode function on this and check the result with the 
+	First we decode the hex encoded bytes and then run the encode function on this and check the result with the
 	corresponding Base64url encoded data.
- */
+*/
 func TestEncode(t *testing.T) {
-	testBytes := make([]byte, len(HEXEncoded) / 2)
+	testBytes := make([]byte, len(HEXEncoded)/2)
 	hex.Decode(testBytes, []byte(HEXEncoded))
 	resultStr := base64url.Encode(testBytes)
-	if(Base64URLEncoded != resultStr) {
+	if Base64URLEncoded != resultStr {
 		t.Errorf(Base64URLEncoded + " not equal with " + resultStr)
 	}
 }
@@ -29,16 +30,22 @@ func TestEncode(t *testing.T) {
 	Test base64url.Decode function using a hex encoded string corresponding to an Base64URLEncoded string.
 	First we decode the Base64URLEncoded string checking the error condition so we have a correctly encoded string.
 	Then we hex encode the result and check it against the corresponding hex encoded string.
- */
+*/
 func TestDecode(t *testing.T) {
 	resultBytes, err := base64url.Decode(Base64URLEncoded)
-	if(err != nil) {
+	if err != nil {
 		t.Errorf("Could not decode Base64URLEncoded string: ", err)
 	}
-	hexEncodedBytes := make([]byte, len(resultBytes) * 2)
+	hexEncodedBytes := make([]byte, len(resultBytes)*2)
 	hex.Encode(hexEncodedBytes, resultBytes)
 	resultStr := string(hexEncodedBytes)
-	if(HEXEncoded != resultStr) {
-		t.Errorf(HEXEncoded + " not equal with " + resultStr)		
+	if HEXEncoded != resultStr {
+		t.Errorf(HEXEncoded + " not equal with " + resultStr)
+	}
+
+	// Should error when an illegal character is encountered.
+	resultBytes, err = base64url.Decode(Base64URLEncoded + "+")
+	if err == nil {
+		t.Errorf("Should throw error when an illegal character is encountered.")
 	}
 }
