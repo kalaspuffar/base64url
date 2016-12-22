807f16b3745188ce0a5dcde7779043e3ba385c0c
Daniel <kalaspuffar@users.noreply.github.com>
2016-12-22 08:17:49 +0100

    Update base64url.go
    Spell check fix.

--- base64url.go
+++ base64url.go
@@ -1,7 +1,7 @@
 package base64url
 
 /*
-	Implementation of base64url removing the traling = and also changing +,/ to the more
+	Implementation of base64url removing the trailing = and also changing +,/ to the more
 	url friendly -,_ characters.
 
 	The decode implementation is reversing the process before running DecodeString from the base64 package.
